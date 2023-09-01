120a0e26f8af8f97a8f5dc7eb2fedcd4a7961efb
Tushn <artur.fhtagn@gmail.com>
2023-08-31 22:20:19 -0300

    Add PWA

--- serviceworker.js
+++ serviceworker.js
@@ -1,21 +1,35 @@
-var staticCacheName = "pwa";
+var staticCacheName = "pwa_dice";
+
+const filesToCache = [
+	'./index.html',
+	'./css/main.css',
+	'./dice.html',
+	'./css/roller.css',
+	'./images/lhama_closed.png',
+	'./images/lhama_opened.png',
+	'./images/bat.png',
+	'./images/rabbit.png',
+	'./images/tejo.png',
+	'./images/mountain.jpg',
+	'./images/mountainnight.jpg'
+];
  
 self.addEventListener("install", function (e) {
-	console.log("eita")
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-		console.log("euta")
-      return cache.addAll(["/"]);
-    })
-  );
+	e.waitUntil(
+		caches.open(staticCacheName).then(function (cache) {
+			// return cache.addAll(["/"]);
+			console.log('ios');
+			return cache.addAll(filesToCache);
+		})
+	);
 });
  
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
+	console.log(event.request.url);
  
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
 });
